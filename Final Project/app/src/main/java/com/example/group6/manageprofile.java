727cad18e1e168f7ae866a95a18578e8864ea8fe
jooannlee99 <CD19068@student.ump.edu.my>
2022-06-08 15:30:52 +0800

    Final_Project_Mobile_Apps

---
+++ Final Project/app/src/main/java/com/example/group6/manageprofile.java
@@ -0,0 +1,14 @@
+package com.example.group6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manageprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manageprofile);
+    }
+}
